5e1f6a1c153ceb86864cf88dee34152b64523e52
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-06-07 19:24:01 -0400

    commit tree 7165849cda45be85479c9173e9a9f180e996c642

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var customerName = customerName.toUpperCase();
+
+function upperCaseCustomerName() {
+   return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'ff';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'hhh';
+}
